37bf5a1b6faca49e7f096cc013ebadb26649f0e5
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-28 19:23:29 +0200

    new Build

---
+++ CocoIsland/App/CocoIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoIslandApp.swift
+//  CocoIsland
+//
+//  Created by Serhii Babchuk on 28.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CocoIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CILaunchView()
+        }
+    }
+}
